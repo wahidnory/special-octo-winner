7d77c65e7110acef15dd21dd85250f35cdbfaa68
Wahid <wahid@elgentos.nl>
2021-11-28 19:13:30 +0100

    Adjust config for heroku

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,11 @@
 import {defineConfig} from 'vite';
 import hydrogen from '@shopify/hydrogen/plugin';
 
-import shopifyConfig from './shopify.config';
-
-if(!shopifyConfig) {
-  const shopifyConfig = {
-    slocale: 'nl-nl',
-    storeDomain: process.env.storefrontDomain, 
-    storefrontToken: process.env.storefrontToken,
-    graphqlApiVersion: 'unstable',
-  }
+const shopifyConfig = {
+  slocale: 'nl-nl',
+  storeDomain: process.env.storefrontDomain, 
+  storefrontToken: process.env.storefrontToken,
+  graphqlApiVersion: 'unstable',
 }
 
 // https://vitejs.dev/config/
